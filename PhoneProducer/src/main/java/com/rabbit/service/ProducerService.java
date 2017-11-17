c4da73da747aa7ea47a4c093f89a4761092585bd
katpap <katpap03@gmail.com>
2017-11-17 10:51:39 +0200

    Update ProducerService.java

--- PhoneProducer/src/main/java/com/rabbit/service/ProducerService.java
+++ PhoneProducer/src/main/java/com/rabbit/service/ProducerService.java
@@ -14,7 +14,7 @@ import com.rabbit.model.PhoneMessage;
 import com.rabbit.util.PhoneCountryCatalog;
 
 @Service	
-public class ProducerService {
+public class ProducerService implements IProducerService{
 
 private static final Logger log = LoggerFactory.getLogger(ProducerService.class);
     
